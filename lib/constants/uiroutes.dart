0ac42565e76d811d8dfcfb0dda098ddfcab92a2f
Gr11n R00bot <Gr11nR00b0t@gmail.com>
2020-07-02 18:34:47 +0300

    fix1

--- lib/constants/uiroutes.dart
+++ lib/constants/uiroutes.dart
@@ -8,6 +8,7 @@ class UIRoutes {
   static const String signIn = '/signin';
   static const String usePinCode = '/signin/usepincode';
 
+
   static Map<String, Widget Function(BuildContext)> getRoutes () {
     return <String, WidgetBuilder> {
       signIn: (BuildContext context) => SignInPage(),
